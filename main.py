e19d42527075fadf2898d2526b2bc0fa827cf7cf
miguel medina <l22121041@morelia.tecnm.mx>
2024-09-11 22:01:09 -0600

    agregue archivo python

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-# main.py
+from circulo import Circulo
 
-from coche import Coche
+circulo_uno = Circulo(5)
+circulo_dos = Circulo(12)
+circulo_tres = Circulo(8)
 
-def main():
-    # Crear tres objetos de tipo Coche
-    coche1 = Coche("Toyota", "Corolla", 2015)
-    coche2 = Coche("Honda", "Civic", 2018)
-    coche3 = Coche("Ford", "Focus", 2020)
-    
-    # Mostrar información de cada coche
-    coches = [coche1, coche2, coche3]
-    for coche in coches:
-        coche.mostrar_informacion()
-        print(f"Edad del coche: {coche.calcular_edad_del_coche(2024)} años\n")
-
-if __name__ == "__main__":
-    main()
+print("\n ------------------------------------ \n")
+print("El area del circulo es: ", circulo_uno.calcular_area())
+print("El area del circulo es: ", circulo_uno.calcular_perimetro())
+print("\n ------------------------------------ \n")
+print("El area del circulo es: ", circulo_dos.calcular_area())
+print("El area del circulo es: ", circulo_dos.calcular_perimetro())
+print("\n ------------------------------------ \n")
+print("El area del circulo es: ", circulo_tres.calcular_area())
+print("El area del circulo es: ", circulo_tres.calcular_perimetro())
